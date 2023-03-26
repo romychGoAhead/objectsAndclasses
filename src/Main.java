ef1b190e976b9fb25825cb3503a3a7d871a437a7
eremi <ereminr83@mail.ru>
2023-03-26 14:56:34 +0700

    for commit

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,11 @@ public class Main {
         System.out.println("Задание 1");
         System.out.println();
 
-        Author pushkin = new Author("Александр", "Пушкин");
+        Author pushkin = new Author("Александр ", "Пушкин");
         Book dubrovsky = new Book("Дубровский", pushkin, 1833);
 
         System.out.println("Книга = " + dubrovsky.getTitle());
-        System.out.println("Автор = " + dubrovsky.getAuthor());
+        System.out.println("Автор = " + pushkin.getName() + pushkin.getSurname());
         System.out.println("Год издания = " + dubrovsky.getYear());
         print();
 
@@ -19,11 +19,11 @@ public class Main {
         System.out.println("Год издания = " + dubrovsky.getYear());
         print();
 
-        Author gogol = new Author("Николай", "Гоголь");
+        Author gogol = new Author("Николай ", "Гоголь");
         Book Viy = new Book("Вий", gogol, 1835);
 
         System.out.println("Книга = " + Viy.getTitle());
-        System.out.println("Автор = " + gogol);
+        System.out.println("Автор = " + gogol.getName() + gogol.getSurname());
         System.out.println("Год издания = " + Viy.getYear());
         print();
     }
